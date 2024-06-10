6c6320e8bfa4dca59ff04b83f1e4ce7a7c3bcb15
Denis Lozhnikov <den.lozhnikov@bk.ru>
2024-06-10 18:12:17 +0500

    10.06

---
+++ payment/gll.js
@@ -0,0 +1,44 @@
+document.getElementById("box1").addEventListener("click", function() {
+  var popup = document.getElementById("popupText");
+  popup.parentElement.classList.add("show");
+
+  setTimeout(function() {
+    popup.parentElement.classList.remove("show");
+  }, 3000);
+});
+
+document.getElementById("box2").addEventListener("click", function() {
+  var popup = document.getElementById("popupText");
+  popup.parentElement.classList.add("show");
+
+  setTimeout(function() {
+    popup.parentElement.classList.remove("show");
+  }, 3000);
+});
+
+document.getElementById("box3").addEventListener("click", function() {
+  var popup = document.getElementById("popupText");
+  popup.parentElement.classList.add("show");
+
+  setTimeout(function() {
+    popup.parentElement.classList.remove("show");
+  }, 3000);
+});
+
+document.getElementById("box4").addEventListener("click", function() {
+  var popup = document.getElementById("popupText");
+  popup.parentElement.classList.add("show");
+
+  setTimeout(function() {
+    popup.parentElement.classList.remove("show");
+  }, 3000);
+});
+
+document.getElementById("boxno").addEventListener("click", function() {
+  var popup = document.getElementById("popupTextno");
+  popup.parentElement.classList.add("show");
+
+  setTimeout(function() {
+    popup.parentElement.classList.remove("show");
+  }, 3000);
+});
